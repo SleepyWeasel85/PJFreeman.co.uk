bfadb0e1319fabfda0c614ea2d8498e0204d9b70
Greg Bowler <greg.bowler@g105b.com>
2016-06-14 20:29:19 +0100

    Start of new design

---
+++ vendor/brightflair/php.gt/src/Core/Exception/UndefinedVariableException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ *
+ * PHP.Gt (http://php.gt)
+ * @copyright Copyright Ⓒ 2015 Bright Flair Ltd. (http://brightflair.com)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace Gt\Core\Exception;
+
+class UndefinedVariableException extends GtException {}#
---
+++ vendor/brightflair/php.gt/src/Data/Store.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Represents a table-like structure and provides some basic interface methods
+ * for typical data manipulation.
+ *
+ * PHP.Gt (http://php.gt)
+ * @copyright Copyright Ⓒ 2015 Bright Flair Ltd. (http://brightflair.com)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace Gt\Data;
+
+interface Store {
+
+public function getAll();
+
+// TODO: Add more useful interface functions here.
+
+}#
